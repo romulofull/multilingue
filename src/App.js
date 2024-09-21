8172c5d4e898fa8536314fafcf098e05766ea26e
romulofull <romulo.castro@gmail.com>
2024-09-20 22:37:55 -0500

    primera version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './index.css';
 
-function App() {
+const App = () => {
+  const [texto, setTexto] = useState('');
+  const [traducciones, setTraducciones] = useState({});
+  const idiomas = ['de', 'it', 'en', 'pt', 'fr', 'he'];
+
+  const traducirTexto = async () => {
+    const promesas = idiomas.map(async (idioma) => {
+      const respuesta = await axios.get('https://api.mymemory.translated.net/get', {
+        params: {
+          q: texto,
+          langpair: `es|${idioma}`,
+        },
+      });
+      return { [idioma]: respuesta.data.responseData.translatedText };
+    });
+
+    const resultados = await Promise.all(promesas);
+    const traduccionesCombinadas = resultados.reduce((acumulador, actual) => ({ ...acumulador, ...actual }), {});
+    setTraducciones(traduccionesCombinadas);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="title">Traductor de Español a 6 Idiomas</h1>
+      <textarea
+        className="input-textarea"
+        value={texto}
+        onChange={(e) => setTexto(e.target.value)}
+        placeholder="Escribe tu frase larga aquí"
+      />
+      <button className="translate-button" onClick={traducirTexto}>Traducir</button>
+      <h2 className="subtitle">Traducciones:</h2>
+      <ul className="translation-list">
+        {Object.entries(traducciones).map(([idioma, textoTraducido]) => (
+          <li key={idioma}>
+            {idioma === 'de' && 'Alemán'}
+            {idioma === 'it' && 'Italiano'}
+            {idioma === 'en' && 'Inglés'}
+            {idioma === 'pt' && 'Portugués'}
+            {idioma === 'fr' && 'Francés'}
+            {idioma === 'he' && 'Hebreo'}: {textoTraducido}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
